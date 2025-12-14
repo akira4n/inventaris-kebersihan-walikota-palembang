c6fd95ee469881e90c6fd003e4ac35ec6a9e08fd
akira4n <yobiijo94@gmail.com>
2025-12-14 14:31:41 +0700

    feat(User & Pengajuan): tambah soft delete untuk fix bentrok foreign key

--- app/Models/Pengajuan.php
+++ app/Models/Pengajuan.php
@@ -22,6 +22,6 @@ class Pengajuan extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 }
